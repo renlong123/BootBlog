c5f64ad0344c610025da73fe8f724496eda6072f
renlong123 <1720476708@qq.com>
2020-11-12 17:30:08 +0800

    使用springboot initializer 搭建了项目的基本框架

---
+++ src/main/java/com/bootblog/BootblogApplication.java
@@ -0,0 +1,13 @@
+package com.bootblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootblogApplication.class, args);
+    }
+
+}
